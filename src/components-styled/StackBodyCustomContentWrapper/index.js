5683acc53c3aef755ffd6ccd4f048b512f1d4f59
Igor Dzhebyan <igor@dzhebyan.net>
2017-02-14 12:53:13 +0200

    Update index.js
    Fix for double scroll bar on iframe
    Fix for iframe positioning issue

--- src/components-styled/StackBodyCustomContentWrapper/index.js
+++ src/components-styled/StackBodyCustomContentWrapper/index.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import { STACK_HEADER_HEIGHT } from '../../utils/styles';
 
 export default styled.div`
-  overflow-x: hidden;
-  overflow-y: auto;
+  overflow: hidden;
   height: ${props => props.pageHeight - STACK_HEADER_HEIGHT}px;
 `;
