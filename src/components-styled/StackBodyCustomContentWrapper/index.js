79c6b0a17d41e19119cfeaa2c20e9573d68fae01
Rostyslav Khanas <rtkhanas@gmail.com>
2017-02-16 16:55:41 +0900

    Fix scrolling

--- src/components-styled/StackBodyCustomContentWrapper/index.js
+++ src/components-styled/StackBodyCustomContentWrapper/index.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { STACK_HEADER_HEIGHT } from '../../utils/styles';
 
 export default styled.div`
-  overflow: hidden;
+  overflow-x: hidden;
+  overflow-y: auto;
   height: ${props => props.pageHeight - STACK_HEADER_HEIGHT}px;
 `;
