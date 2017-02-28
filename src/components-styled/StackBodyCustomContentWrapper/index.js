229062b922b048336612458324891ff3d374f155
Igor Dzhebyan <id@try.cards>
2017-02-28 23:34:36 +0900

    Add kinetic scroll for iOS devices

--- src/components-styled/StackBodyCustomContentWrapper/index.js
+++ src/components-styled/StackBodyCustomContentWrapper/index.js
@@ -3,6 +3,7 @@ import { STACK_HEADER_HEIGHT } from '../../utils/styles';
 
 export default styled.div`
   overflow-x: hidden;
-  overflow-y: auto;
+  overflow-y: scroll;
+  -webkit-overflow-scrolling: touch;
   height: ${props => props.pageHeight - STACK_HEADER_HEIGHT}px;
 `;
